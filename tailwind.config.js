5858a11811d4255a1c05083eb13091c6325fee3b
Angel Martinez <angel.martineztrc@gmail.com>
2021-06-30 13:24:14 -0500

    feat(root):  set up a vitejs project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
